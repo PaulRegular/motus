a577d0725643176562fdd3c4d090e3ccbda08efc
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2018-05-04 10:32:01 -0230

    Add t and cauchy distribution options to obs_error model

--- analysis/001_obs_error.r
+++ analysis/001_obs_error.r
@@ -5,42 +5,54 @@ library(ggplot2)
 library(data.table)
 
 ## Truncate n to simplify estimation
-sync_dat$n[sync_dat$n >= 10] <- 10
+dat <- sync_dat
+dat$n[dat$n >= 10] <- 10
+
+## Couple additions for plotting
+dat$month <- data.table::month(dat$DATETIME)
+dat$week <- data.table::week(dat$DATETIME)
+dat$date <- as.POSIXct(as.IDate(dat$DATETIME))
+dat$yday <- data.table::yday(dat$DATETIME)
 
 ## Combine array and n to make the factor levels for the model
-l <- unique(sync_dat[, list(array, n)])
+l <- unique(dat[, list(array, n)])
 l <- l[order(l$array, l$n),]
 l$levels <- paste0(l$array, "-", l$n)
-sync_dat$array_n <- factor(paste0(sync_dat$array, "-", sync_dat$n), levels = l$levels)
+dat$array_n <- factor(paste0(dat$array, "-", dat$n), levels = l$levels)
 
 # ## Quick views
-# ggplot(sync_dat) +
+# ggplot(dat) +
 #     geom_violin(aes(x = month, group = month, y = dev_easting)) +
 #     facet_grid(year ~ location, scales = "free")
-# ggplot(sync_dat) +
+# ggplot(dat) +
 #     geom_violin(aes(x = week, group = week, y = dev_easting)) +
 #     facet_grid(year ~ location, scales = "free")
-# ggplot(sync_dat) +
+# ggplot(dat) +
 #     geom_violin(aes(x = yday, group = yday, y = dev_easting)) +
 #     facet_grid(year ~ location, scales = "free")
-# ggplot(sync_dat) +
+# ggplot(dat) +
 #     geom_violin(aes(x = n, group = n, y = dev_easting)) +
 #     facet_grid(year ~ location, scales = "free")
-# ggplot(sync_dat) +
+# ggplot(dat) +
 #     geom_violin(aes(x = n, group = n, y = dev_northing)) +
 #     facet_grid(year ~ location, scales = "free")
 
 ## Exclude early records because there was extensive noise
-sync_dat <- sync_dat[sync_dat$yday >= 240, ]
+dat <- dat[dat$yday >= 240, ]
 
 ## Set-up TMB data and par inits
-tmb_data <- list(lon = unname(sync_dat$dev_easting),
-                 lat = unname(sync_dat$dev_northing),
-                 n_i = as.numeric(sync_dat$array_n) - 1)
-tmb_pars <- list(log_tau_lon = rep(0, length(unique(tmb_data$n_i))),
+tmb_data <- list(lon = unname(dat$dev_easting),
+                 lat = unname(dat$dev_northing),
+                 n_i = as.numeric(dat$array_n) - 1,
+                 dist = 1)
+tmb_pars <- list(log_sd_lon = rep(0, length(unique(tmb_data$n_i))),
+                 log_sd_lat = rep(0, length(unique(tmb_data$n_i))),
+                 log_tau_lon = rep(0, length(unique(tmb_data$n_i))),
                  log_nu_lon = rep(log(3), length(unique(tmb_data$n_i))),
                  log_tau_lat = rep(0, length(unique(tmb_data$n_i))),
-                 log_nu_lat = rep(log(3), length(unique(tmb_data$n_i))))
+                 log_nu_lat = rep(log(3), length(unique(tmb_data$n_i))),
+                 log_scale_lon = rep(0, length(unique(tmb_data$n_i))),
+                 log_scale_lat = rep(0, length(unique(tmb_data$n_i))))
 tmb_map <- list(log_nu_lon = rep(factor(NA), length(tmb_pars$log_nu_lon)),
                 log_nu_lat = rep(factor(NA), length(tmb_pars$log_nu_lat)))   ## df fixed to 3 to account for heavy tails while maintaining statistical properities (mean etc.)
 str(tmb_data)
@@ -50,31 +62,71 @@ str(tmb_pars)
 compile("analysis/obs_error.cpp")
 dyn.load(dynlib("analysis/obs_error"))
 
-## Generate objective function
-obj <- MakeADFun(tmb_data, tmb_pars, DLL = "obs_error", map = tmb_map)
+## Fit normal distribution -----------------------------------------------------
+
+tmb_data$dist <- 0
+obj <- MakeADFun(tmb_data, tmb_pars[c("log_sd_lon", "log_sd_lat")],
+                 DLL = "obs_error")
 
 ## Minimize the objective function using nlminb in R
 opt <- nlminb(obj$par, obj$fn, obj$gr,
               control = list(eval.max = 2000, iter.max = 2000))
-rep <- obj$report()
+dnorm_rep <- obj$report()
 sdrep <- sdreport(obj)
 sdrep
 
+## Fit t-distribution ----------------------------------------------------------
+
+tmb_data$dist <- 1
+obj <- MakeADFun(tmb_data, tmb_pars[c("log_tau_lon", "log_nu_lon", "log_tau_lat", "log_nu_lat")],
+                 DLL = "obs_error", map = tmb_map)
+
+## Minimize the objective function using nlminb in R
+opt <- nlminb(obj$par, obj$fn, obj$gr,
+              control = list(eval.max = 2000, iter.max = 2000))
+dt_rep <- obj$report()
+sdrep <- sdreport(obj)
+sdrep
+
+## Fit Cauchy distribution -----------------------------------------------------
+
+tmb_data$dist <- 2
+obj <- MakeADFun(tmb_data, tmb_pars[c("log_scale_lon", "log_scale_lat")],
+                 DLL = "obs_error")
+
+## Minimize the objective function using nlminb in R
+opt <- nlminb(obj$par, obj$fn, obj$gr,
+              control = list(eval.max = 2000, iter.max = 2000))
+dcauchy_rep <- obj$report()
+sdrep <- sdreport(obj)
+sdrep
+
+## Compile results -------------------------------------------------------------
+
 ## Table
-obs_error <- data.frame(array_n = levels(sync_dat$array_n),
-                        tau_lon = rep$tau_lon, nu_lon = rep$nu_lon,
-                        tau_lat = rep$tau_lat, nu_lat = rep$nu_lat)
+obs_error <- data.frame(array_n = levels(dat$array_n),
+                        sd_lon = dnorm_rep$sd_lon, sd_lat = dnorm_rep$sd_lat,
+                        tau_lon = dt_rep$tau_lon, nu_lon = dt_rep$nu_lon,
+                        tau_lat = dt_rep$tau_lat, nu_lat = dt_rep$nu_lat,
+                        scale_lon = dcauchy_rep$scale_lon,
+                        scale_lat = dcauchy_rep$scale_lat)
 array_n <- do.call(rbind, strsplit(as.character(obs_error$array_n), "-"))
 obs_error$location <- array_n[, 1]
 obs_error$year <- as.numeric(array_n[, 2])
 obs_error$n <- as.numeric(array_n[, 3])
 obs_error
 ggplot(obs_error, aes(x = n)) +
+    geom_point(aes(y = sd_lon)) +
+    facet_grid(location ~ year)
+ggplot(obs_error, aes(x = n)) +
   geom_point(aes(y = tau_lon)) +
   facet_grid(location ~ year)
 ggplot(obs_error, aes(x = n)) +
   geom_point(aes(y = nu_lon)) +
   facet_grid(location ~ year)
+ggplot(obs_error, aes(x = n)) +
+    geom_point(aes(y = scale_lon)) +
+    facet_grid(location ~ year)
 obs_error[, c("location", "year", "n")] <- NULL
 saveRDS(obs_error, file = "analysis/obs_error.rds")
 
--- analysis/obs_error.cpp
+++ analysis/obs_error.cpp
@@ -1,39 +1,91 @@
 #include <TMB.hpp>
 
 template<class Type>
+Type dcauchy(Type x, Type location, Type scale, int give_log){
+    Type pi = 3.1415926535897932384;
+    Type y = (x - location) / scale;
+    Type res = Type(1.0) / (pi * scale * (Type(1.0) + y * y));
+    if(give_log) {
+        return(log(res));
+    } else {
+        return(res);
+    }
+}
+VECTORIZE4_ttti(dcauchy);
+
+template<class Type>
 Type objective_function<Type>::operator() ()
 {
-  
-  // Input data
+
   DATA_VECTOR(lon);
   DATA_VECTOR(lat);
   DATA_IVECTOR(n_i);
-  
-  // Parameters
-  PARAMETER_VECTOR(log_tau_lon);
-  PARAMETER_VECTOR(log_nu_lon);
-  PARAMETER_VECTOR(log_tau_lat);
-  PARAMETER_VECTOR(log_nu_lat);
+  DATA_INTEGER(dist); // toggle for distribution to fit
+
   Type mu = 0.0;
-  
-  // Transformations
-  vector<Type> tau_lon = exp(log_tau_lon);
-  vector<Type> nu_lon = exp(log_nu_lon);
-  vector<Type> tau_lat = exp(log_tau_lat);
-  vector<Type> nu_lat = exp(log_nu_lat);
-  
-  // Model
+
   parallel_accumulator<Type> nll(this);
-  for(int i = 0; i < lon.size(); ++i){
-    nll -= dt((lon(i) - mu)/tau_lon(n_i(i)), nu_lon(n_i(i)), true) - log(tau_lon(n_i(i)));
-    nll -= dt((lat(i) - mu)/tau_lat(n_i(i)), nu_lat(n_i(i)), true) - log(tau_lat(n_i(i)));
+
+  if (dist == 0) { // normal distribution
+
+      PARAMETER_VECTOR(log_sd_lon);
+      PARAMETER_VECTOR(log_sd_lat);
+
+      vector<Type> sd_lon = exp(log_sd_lon);
+      vector<Type> sd_lat = exp(log_sd_lat);
+
+      for (int i = 0; i < lon.size(); ++i){
+          nll -= dnorm(lon(i), mu, sd_lon(n_i(i)), true);
+          nll -= dnorm(lat(i), mu, sd_lat(n_i(i)), true);
+      }
+
+      REPORT(sd_lon);
+      REPORT(sd_lat);
+
   }
-  
-  // Reports
-  REPORT(tau_lon);
-  REPORT(nu_lon);
-  REPORT(tau_lat);
-  REPORT(nu_lat);
+
+  if (dist == 1) { // t-distribution
+
+      PARAMETER_VECTOR(log_tau_lon);
+      PARAMETER_VECTOR(log_nu_lon);
+      PARAMETER_VECTOR(log_tau_lat);
+      PARAMETER_VECTOR(log_nu_lat);
+
+      vector<Type> tau_lon = exp(log_tau_lon);
+      vector<Type> nu_lon = exp(log_nu_lon);
+      vector<Type> tau_lat = exp(log_tau_lat);
+      vector<Type> nu_lat = exp(log_nu_lat);
+
+      for (int i = 0; i < lon.size(); ++i){
+          nll -= dt((lon(i) - mu)/tau_lon(n_i(i)), nu_lon(n_i(i)), true) - log(tau_lon(n_i(i)));
+          nll -= dt((lat(i) - mu)/tau_lat(n_i(i)), nu_lat(n_i(i)), true) - log(tau_lat(n_i(i)));
+      }
+
+      REPORT(tau_lon);
+      REPORT(nu_lon);
+      REPORT(tau_lat);
+      REPORT(nu_lat);
+
+  }
+
+  if (dist == 2) { // Cauchy distribution
+
+      PARAMETER_VECTOR(log_scale_lon);
+      PARAMETER_VECTOR(log_scale_lat);
+
+      vector<Type> scale_lon = exp(log_scale_lon);
+      vector<Type> scale_lat = exp(log_scale_lat);
+
+      for(int i = 0; i < lon.size(); ++i){
+          nll -= dcauchy(lon(i), mu, scale_lon(n_i(i)), true);
+          nll -= dcauchy(lat(i), mu, scale_lat(n_i(i)), true);
+      }
+
+      REPORT(scale_lon);
+      REPORT(scale_lat);
+
+  }
+
   return nll;
-  
+
 }
