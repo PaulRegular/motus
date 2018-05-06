9786a6556499835b22e855091a965af724945e36
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2018-05-06 07:55:46 -0230

    Test different scalings to try and improve convergence

--- analysis/002_tracks.r
+++ analysis/002_tracks.r
@@ -49,7 +49,7 @@ res$sd_rep
 hist(res$track$gamma_est, breaks = 100)
 hist(res$track$logit_gamma_est, breaks = 100)
 
-# plot_track(res$track)
+plot_track(res$track)
 p_lon <- plot_trend(res$track, y_name = "lon")
 p_lat <- plot_trend(res$track, y_name = "lat")
 p_gamma <- plot_trend(res$track, y_name = "gamma")
@@ -57,13 +57,41 @@ subplot(p_lon, p_lat, p_gamma, nrows = 3, shareX = TRUE)
 unique(res$track$TRANSMITTER)
 
 
-## Try fitting to all individuals
-ids <- unique(tracks$TRANSMITTER)
-tfits <- nfits <- cfits <- vector(mode = "list", length = length(ids))
-names(tfits) <- names(nfits) <- names(cfits) <- ids
-for (id in ids) {
-    track <- tracks[tracks$TRANSMITTER == id, ]
-    tfits[[id]] <- try(fit_ssm(track, dist = "t"))
-    nfits[[id]] <- try(fit_ssm(track, dist = "normal"))
-    cfits[[id]] <- try(fit_ssm(track, dist = "cauchy"))
-}
+system.time({
+    ## Try fitting to all individuals
+    set.seed(123)
+    ids <- sample(unique(tracks$TRANSMITTER), 100)
+    tfits <- nfits <- cfits <- vector(mode = "list", length = length(ids))
+    names(tfits) <- names(nfits) <- names(cfits) <- ids
+    success <- 0
+    for (i in seq_along(ids)) {
+        track <- tracks[tracks$TRANSMITTER == ids[i], ]
+        tfits[[i]] <- try(fit_ssm(track, dist = "t", scale = 5000, silent = TRUE))
+        if (class(tfits[[i]]) != "try-error") success <- success + 1
+        # nfits[[i]] <- try(fit_ssm(track, dist = "normal", scale = 1000, silent = TRUE))
+        # cfits[[i]] <- try(fit_ssm(track, dist = "cauchy", scale = 1000, silent = TRUE))
+        cat(paste(i, "out of", length(ids), "\n"))
+        cat(paste0(round(success / i * 100), "% converged\n"))
+    }
+})
+
+mean(sapply(tfits, class) != "try-error")
+mean(sapply(nfits, class) != "try-error")
+mean(sapply(cfits, class) != "try-error")
+
+success <- names(which(sapply(cfits, class) != "try-error"))
+res <- cfits[[success[14]]]
+plot_track(res$track)
+plot_trend(res$track, y_name = "gamma")
+
+success <- names(which(sapply(tfits, class) != "try-error"))
+res <- tfits[[success[76]]]
+plot_track(res$track, title = unique(res$track$TRANSMITTER))
+plot_trend(res$track, y_name = "gamma")
+hist(res$track$gamma_est, breaks = 100, xlab = "gamma")
+unique(res$track$TRANSMITTER)
+## good examples: "Carson-2016-53231", "Lilly-2016-53174", "Lilly-2016-53240"
+## should think about changes in angle: "Carson-2017-53263"
+
+## Add start and end label to plot_track
+## Allow for covariate effects on gamma and model error as an autocorrelated process (AR1)
